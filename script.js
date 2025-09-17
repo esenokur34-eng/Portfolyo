2bfe65fa2f088e70648cb830fdb8a99c487f4c5f
Eylul Okur <esenokur34@gmail.com>
2025-09-17 18:22:01 +0300

    İlk commit=portfolyo sitesi

---
+++ script.js
@@ -0,0 +1,11 @@
+// Sayfa kaydırma fonksiyonu
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+  }
+  
+  // İletişim formu
+  function sendMessage(event) {
+    event.preventDefault();
+    alert("Mesajınız gönderildi.54");
+  }
+  
