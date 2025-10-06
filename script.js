dbaf6db326ab24a85f0da93fa9cd9f30fa8ac458
Eylul Okur <esenokur34@gmail.com>
2025-10-06 19:35:47 +0300

    Js'de sayfa yönlendirme yapıldı, yeni sayfalar oluşturuldu.

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// Sayfa kaydırma fonksiyonu
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-  }
-  
-  // İletişim formu
-  function sendMessage(event) {
-    event.preventDefault();
-    alert("Mesajınız gönderildi.54");
-  }
-  
+function openPage(pageName) {
+  window.location.href = pageName;
+}
